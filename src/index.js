f5ad4ca0f0186c833a5a52fbd1536023f8f27078
aylilove <zatoboo@mail.ru>
2024-03-24 01:34:40 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import { Provider } from "react-redux";
+import store, { persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router}></RouterProvider>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+);
---
+++ src/pages/SelectLevelPage/SelectLevelPage.jsx
@@ -0,0 +1,55 @@
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "../../components/Button/Button";
+import styles from "./SelectLevelPage.module.css";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleDifficultyModeReducer } from "../../store/gameSlice";
+
+export function SelectLevelPage() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const LEVELS = [1, 2, 3];
+  const [level, setLevel] = useState(1);
+  const { difficultyMode } = useSelector(state => state.game);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.modal}>
+        <h1 className={styles.title}>Выбери сложность</h1>
+        <ul className={styles.levels}>
+          {LEVELS.map(item => {
+            return (
+              <li className={`${styles.level} ${LEVELS[item - 1] === level && styles.selected}`} key={item}>
+                <button className={styles.levelLink} onClick={() => setLevel(item)}>
+                  {item}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+
+        <label className={styles.subtitle}>
+          <input
+            type="checkbox"
+            name="attempts"
+            className={styles.input}
+            onChange={() => dispatch(toggleDifficultyModeReducer())}
+            defaultChecked={difficultyMode}
+          />
+          Cчетчик попыток
+        </label>
+
+        <Button
+          onClick={() => {
+            navigate(`/game/${level * 3}`);
+          }}
+        >
+          Начать игру
+        </Button>
+        <Link className={styles.link} to="/leaderboard">
+          Перейти к лидерборду
+        </Link>
+      </div>
+    </div>
+  );
+}
